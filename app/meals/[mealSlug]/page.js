1232a1de9994d04f6005f6301eaa39f30a19cde6
James Cooper <cooper.jm@outlook.com>
2025-11-10 21:05:13 -0500

    97

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,7 +2,9 @@ export default function Share({ params }) {
    console.log(params);
    return (
       <main>
-         <h1 style={{ color: "white", textAlign: "center" }}>{params.slug}</h1>
+         <h1 style={{ color: "white", textAlign: "center" }}>
+            {params.mealSlug}
+         </h1>
       </main>
    );
 }
