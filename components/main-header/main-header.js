d6ca4eef1b6d8e1ce84424a3b3ad792fd092db43
James Cooper <cooper.jm@outlook.com>
2025-11-12 20:35:34 -0500

    106

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,8 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
-import MainHeaderBackground from "@/components/main-header/main-header-background";
 import logoImg from "@/assets/logo.png";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import Image from "next/image";
+import Link from "next/link";
 import classes from "./main-header.module.css";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
    return (
@@ -17,10 +18,10 @@ export default function MainHeader() {
             <nav className={classes.nav}>
                <ul>
                   <li>
-                     <Link href="/meals">Browse Meals</Link>
+                     <NavLink href="/meals">Browse Meals</NavLink>
                   </li>
                   <li>
-                     <Link href="/community">Foodies Community</Link>
+                     <NavLink href="/community">Foodies Community</NavLink>
                   </li>
                </ul>
             </nav>
